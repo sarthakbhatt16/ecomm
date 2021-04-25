a32e4725057ccac3e6012939c4d54d6e87851e6d
Sarthak Bhatt <sarthakbhatt16@gmail.com>
2021-04-24 21:17:04 -0400

    push five

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 // server.js
 //Install express server
-import express, { static } from "express";
-import { createServer } from "http";
-import { join } from "path";
+const express = require("express");
+const http = require("http");
+const path = require("path");
 const app = express();
 // Serve only the static files from the dist directory
-app.use(static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 const port = process.env.PORT || 8080;
 app.set(port);
-const server = createServer(app);
+const server = http.createServer(app);
 server.listen(port, () => console.log("Runing"));
