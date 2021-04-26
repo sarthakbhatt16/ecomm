112a8a9981aa5acd481c1668879fe7c287901ead
Sarthak Bhatt <sarthakbhatt16@gmail.com>
2021-04-26 14:47:26 -0400

    push seven

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/dist'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
 const port = process.env.PORT || 8080;
 app.set(port);
 const server = http.createServer(app);
